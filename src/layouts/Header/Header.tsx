7c3ca39b039742220896766e0b706930ea26ae61
Oleksandr-Berd <alex.berd86@gmail.com>
2024-11-08 11:28:33 +0200

    start

---
+++ src/layouts/Header/Header.tsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/icons/logo.svg"
+
+const Header:React.FC = () => {
+    return (
+      <header>
+        <img src="../../assets/icons/logo.svg" alt="logo" />
+      </header>
+    );
+}
+ 
+export default Header;
