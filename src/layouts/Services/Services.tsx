54bd3318e79f717879c230dd9319dd2768f9be87
Oleksandr-Berd <alex.berd86@gmail.com>
2024-11-08 15:20:17 +0200

    service menu

--- src/layouts/Services/Services.tsx
+++ src/layouts/Services/Services.tsx
@@ -1,15 +1,27 @@
-import mobilePic from "../../assets/pic/mobile.svg"
+import mobilePic from "../../assets/pic/mobile.svg";
 
-import * as SC from "./ServicesStyled"
+import * as SC from "./ServicesStyled";
 
-const Services:React.FC = () => {
-    return (
-      <SC.ServicesLayout>
-        <SC.ImageCon>
-          <img src={mobilePic} alt="mobile" />
-        </SC.ImageCon>
-      </SC.ServicesLayout>
-    );
-}
- 
+const Services: React.FC = () => {
+  return (
+    <SC.ServicesLayout>
+      <SC.ImageCon>
+        <img src={mobilePic} alt="mobile" />
+      </SC.ImageCon>
+      <SC.Menu>
+        <SC.MenuTitle>Premium EQ</SC.MenuTitle>
+        <p>
+          Get expert-level control with a robust equalizer, volume mixer, and
+          spatial audio. Take your listening experience to a whole new level and
+          access all our incredible features!
+        </p>
+        <h4>
+          <span>$ 4</span>
+          month
+        </h4>
+      </SC.Menu>
+    </SC.ServicesLayout>
+  );
+};
+
 export default Services;
--- src/layouts/Services/ServicesStyled.ts
+++ src/layouts/Services/ServicesStyled.ts
@@ -1,16 +1,49 @@
 import styled from "styled-components";
 
-export const ServicesLayout = styled.div`
-position: relative;
+import backImage from "../../assets/pic/back.svg";
 
-min-height: 380px;
+export const ServicesLayout = styled.div`
+  position: relative;
+
+  min-height: 380px;
+
+  display: flex;
 
   background-color: #191826;
+  background-image: url(${backImage});
+  background-repeat: no-repeat;
+  background-position: 50%;
+
+  border-radius: 12px;
 `;
 
 export const ImageCon = styled.div`
   position: absolute;
-  top: 98px; 
+  top: 98px;
   left: 50%;
   transform: translate(-50%, -50%);
+`;
+
+export const Menu = styled.div`
+  margin-top: 380px;
+
+  padding-left: 36px;
+  padding-top: 48px;
+  padding-right: 37px;
+  padding-bottom: 48px;
+
+  text-align: left;
+
+  background-color: #fa7453;
+  border-radius: 12px;
+`;
+
+export const MenuTitle = styled.h3`
+  margin-bottom: 12px;
+
+  font-family: "IBMbold";
+  font-size: 32px;
+  line-height: 1.25;
+
+  color: #fcfaf9;
 `;
